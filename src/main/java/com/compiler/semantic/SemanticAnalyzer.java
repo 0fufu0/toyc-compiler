adc569f9b5c2447edbd97c31dd347247df9b9245
Liu <liuzhuoying25@gmail.com>
2026-06-23 09:18:59 +0800

    chore: fix build after rebase

---
+++ src/main/java/com/compiler/semantic/SemanticAnalyzer.java
@@ -0,0 +1,12 @@
+package com.compiler.semantic;
+
+import com.compiler.ast.CompUnitNode;
+
+public class SemanticAnalyzer {
+
+    private final SymbolTable symbolTable = new SymbolTable();
+
+    public void analyze(CompUnitNode compUnit) {
+        // TODO: 后面遍历 AST
+    }
+}
